5be0cb9439788070d35d8ca90f4f8090069a4a4f
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-01-31 12:21:58 +0100

    game logic
    - Implemented a Rock, Paper, Scissors game.
    - Added event listeners to buttons for user input.
    - Generated a random choice for the computer.
    - Determined the winner based on game rules.
    - Updated and displayed scores dynamically.

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
 const rpsChoice = ['rock', 'paper', 'scissors']
-let humanScore = 0, computerScore = 0;
+let userScore = 0, computerScore = 0;
+const userScoreEl = document.getElementById('user-score')
+const computerScoreEl = document.getElementById('computer-score')
+const buttons = document.querySelectorAll('button')
+const userChoiceEl = document.getElementById('user-choice')
+const computerChoiceEl = document.getElementById('computer-choice')
+const announcementEl = document.querySelector('.announcement')
+const vsEl = document.querySelector('.vs')
+vsEl.style.display = 'none'
 
+userScoreEl.textContent = userScore
+computerScoreEl.textContent = computerScore
+
+buttons.forEach(button=>{
+  button.addEventListener('click', playRound)
+})
+
+function playRound(e){
+  const computerChoice = getComputerChoice()
+  const userChoice = e.target.textContent.toLowerCase()
+  let resultText = ''
+  
+  computerChoiceEl.textContent = computerChoice
+  userChoiceEl.textContent = userChoice
+  if(userChoice == 'scissors' && computerChoice == 'paper'){
+    resultText = "You won!"
+    userScore ++
+  } else if (userChoice == 'paper' && computerChoice == 'rock'){
+    resultText = "You won!"
+    userScore ++
+  } else if (userChoice == 'rock' && computerChoice == 'scissors'){
+    resultText = "You won!"
+    userScore ++
+  } else if (userChoice == computerChoice){
+    resultText = "It is a tie"
+  } else{
+    resultText = "You Lost!"
+    computerScore ++
+  }
+
+  announcementEl.textContent = resultText
+  userScoreEl.textContent = userScore
+  vsEl.style.display = 'block'
+  computerScoreEl.textContent = computerScore
+}
 function getComputerChoice(){
   const ranNum = Math.floor(Math.random()*3)
   return rpsChoice[ranNum]
 }
 
-function getHumanChoice(){
-  let humanChoice = prompt("What do you choose: 'Rock', 'Paper' or 'Scissors'? Write it below.")
-  if (humanChoice === null) {
-    console.log("You cancelled the prompt. Exiting the game.");
-    return null;  // Or you can return a default value or ask again if preferred
-  }
-  return humanChoice.toLowerCase()
-}
 
-function playRound(humanChoice, computerChoice){
-  // Below handles invalid choice
-  if(!rpsChoice.includes(humanChoice)){
-    console.log("You entered an invalid choice")
-    return
-  }
-  if(humanChoice == 'scissors' && computerChoice == 'paper'){
-    console.log("You won!")
-    humanScore ++
-  } else if (humanChoice == 'paper' && computerChoice == 'rock'){
-    console.log("You won!")
-    humanScore ++
-  } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
-    console.log("You won!")
-    humanScore ++
-  } else if (humanChoice == computerChoice){
-    console.log("It is a tie")
-  } else{
-    console.log("You Lost!")
-    computerScore ++
-  }
-}
+// function playRound(humanChoice, computerChoice){
+//   // Below handles invalid choice
+//   if(!rpsChoice.includes(humanChoice)){
+//     console.log("You entered an invalid choice")
+//     return
+//   }
+// }
 
-function playGame(){
-  const humanChoice = getHumanChoice()
-  if (humanChoice === null) return;
-  const computerChoice = getComputerChoice()
-  playRound(humanChoice, computerChoice)
-}
+// function playGame(){
+//   const humanChoice = getHumanChoice()
+//   if (humanChoice === null) return;
+//   const computerChoice = getComputerChoice()
+//   playRound(humanChoice, computerChoice)
+// }
 
-playGame()
-playGame()
-playGame()
-playGame()
-playGame()
-console.log("Your score: " + humanScore)
-console.log("Computer's score: " + computerScore)
+// playGame()
+// playGame()
+// playGame()
+// playGame()
+// playGame()
+// console.log("Your score: " + humanScore)
+// console.log("Computer's score: " + computerScore)
